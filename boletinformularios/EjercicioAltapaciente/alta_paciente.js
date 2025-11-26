e5e2daf9e1324bcd45e9ac146383c06cb9276f62
fojedaiesruizgijon <franciscojavier.ojeda-cortes@iesruizgijon.com>
2025-11-26 19:57:07 +0100

    empiezo boletin

---
+++ boletinformularios/EjercicioAltapaciente/alta_paciente.js
@@ -0,0 +1,18 @@
+ 
+ /*<form action="">
+        ¿Confirma los términos y condiciones?
+        <input type="checkbox" id="checkbox1">
+        <input type="submit" id="botonConfirmar" value="Confirmar" disabled>
+    </form>*/
+ document.getElementById('mifumador').addEventListener('change', cambiarEstado);
+
+               function cambiarEstado() {
+                   let check = document.getElementById('mifumador').checked;
+            let boton = document.getElementById('micigarrillos');
+
+   if (check) {
+                boton.disabled =false;  
+            } else {
+                boton.disabled =true;  
+            }
+        }
